3494d6b100e53c21f09406ff9f78ed1034469b9b
sakai shunsuke <sakai@ans-web.co.jp>
2011-10-16 16:24:41 +0900

    config モジュールがうまくインクルードできていない問題を修正

--- lib/ans-gem-builder/bundler.rb
+++ lib/ans-gem-builder/bundler.rb
@@ -2,31 +2,33 @@
 
 require 'bundler'
 
-class Ans::Gem::Builder::Bundler < ::Bundler::GemHelper
-  include Config
+module Ans::Gem::Builder
+  class Bundler < ::Bundler::GemHelper
+    include Config
 
-  def install
-    desc "Create tag #{version_tag} and push"
-    task 'release' do
-      release_gem_without_rubygem
+    def install
+      desc "Create tag #{version_tag} and push"
+      task 'release' do
+        release_gem_without_rubygem
+      end
     end
-  end
 
-  def release_gem_without_rubygem
-    guard_clean
-    guard_already_tagged
-    gem_path = build_gem
-    tag_version {
-      git_push
-      gem_push gem_path
-    }
-  end
+    def release_gem_without_rubygem
+      guard_clean
+      guard_already_tagged
+      gem_path = build_gem
+      tag_version {
+        git_push
+        gem_push gem_path
+      }
+    end
 
-  private
+    private
 
-  def gem_push(path)
-    sh "scp #{path} #{gem_host}:#{gem_root}/gems"
-    sh %Q{ssh #{gem_host} "rvm_path=#{remote_rvm_path} #{remote_rvm_path}/bin/rvm-shell '#{remote_ruby_version}' -c 'cd #{gem_root} && gem generate_index'"}
-    Bundler.ui.confirm "Pushed #{name} #{version} to #{gem_host}"
+    def gem_push(path)
+      sh "scp #{path} #{gem_host}:#{gem_root}/gems"
+      sh %Q{ssh #{gem_host} "rvm_path=#{remote_rvm_path} #{remote_rvm_path}/bin/rvm-shell '#{remote_ruby_version}' -c 'cd #{gem_root} && gem generate_index'"}
+      Bundler.ui.confirm "Pushed #{name} #{version} to #{gem_host}"
+    end
   end
 end
